45f6428c7bfc9579218fd6941a336fba240a0d2c
root <root@localhost>
2020-02-08 04:04:09 -0600

    admin security

--- app/Upgrades/Finance/20200205_07_00_add_admin_adminusergroup_id.sql
+++ app/Upgrades/Finance/20200205_07_00_add_admin_adminusergroup_id.sql
@@ -5,7 +5,7 @@ ALTER TABLE admin ADD COLUMN IF NOT EXISTS adminusergroup_id INTEGER default NUL
 
 
 ALTER TABLE ONLY public.admin
-    ADD CONSTRAINT IF NOT EXISTS admin_adminusergroup_fkey FOREIGN KEY (adminusergroup_id) REFERENCES public.adminusergroup(adminusergroup_id);
+    ADD CONSTRAINT  admin_adminusergroup_fkey FOREIGN KEY (adminusergroup_id) REFERENCES public.adminusergroup(adminusergroup_id);
 
 
 UPDATE admin SET adminusergroup_id=(SELECT adminusergroup_id FROM adminusergroup WHERE usergroup=admin.usergroup);
